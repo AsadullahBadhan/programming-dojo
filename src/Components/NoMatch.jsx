2e8bb18d76bd413f2bd4897faf6b1aaa65b2069a
AsadullahBadhan <asadullah.badhan@gmail.com>
2023-01-30 04:25:07 +0600

    no page route content update

--- src/Components/NoMatch.jsx
+++ src/Components/NoMatch.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const NoMatch = () => {
-	return <div>sorry! not found</div>;
+	return (
+		<div className="p-16 mx-auto flex flex-col items-center justify-center">
+			<img
+				src="https://i.postimg.cc/KvHXd23H/undraw-page-not-found-re-e9o6.png"
+				alt="404 not found"
+				className="w-1/2"
+			/>
+			<h1 className="mt-8 text-4xl font-bold">Oops! No page found</h1>
+		</div>
+	);
 };
 
 export default NoMatch;
